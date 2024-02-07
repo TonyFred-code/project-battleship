3f42dbe11a545c064e7aef82673eb604756545bd
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-02-07 06:37:23 +0100

    feat: add test for length property

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -4,8 +4,14 @@ it('create new ship', () => {
   expect(new Ship(2)).not.toBeUndefined();
 });
 
-it.todo('ship has a length');
+it('ship has a length', () => {
+  expect(new Ship(3)).toHaveLength(3);
+});
 
 it.todo('ship stores number of times hit');
 
 it.todo('ship knows whether or not it is sunk');
+
+it.todo('ship hit() function increase number of hits');
+
+it.todo('ship isSunk() function checks if ship is sunk');
