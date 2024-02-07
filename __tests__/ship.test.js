a5d79e2f7a6aecddeb5b6fc023e47b5b4d74ea62
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-02-07 06:32:27 +0100

    feat: add todo tests for ship class

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,5 +1,11 @@
-const Ship = require('../src/ship');
+import Ship from '../src/ship';
 
-test('create new ship', () => {
-  expect(new Ship()).toNotBeUndefined();
+it('create new ship', () => {
+  expect(new Ship(2)).not.toBeUndefined();
 });
+
+it.todo('ship has a length');
+
+it.todo('ship stores number of times hit');
+
+it.todo('ship knows whether or not it is sunk');
