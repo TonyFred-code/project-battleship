e4720e0dfa195bb04e909e39b88759802b283dbe
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-02-07 13:11:38 +0100

    feat: complete tests for methods and properties of ship class

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -8,10 +8,36 @@ it('ship has a length', () => {
   expect(new Ship(3)).toHaveLength(3);
 });
 
-it.todo('ship stores number of times hit');
+it('ship stores number of times hit', () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship.hitsCount).toBe(1);
+});
 
-it.todo('ship knows whether or not it is sunk');
+it('ship knows whether or not it is sunk', () => {
+  const ship = new Ship(1);
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
 
-it.todo('ship hit() function increase number of hits');
+it('disallows zero or lower ship length', () => {
+  const tryErrorShip = () => new Ship(-2);
 
-it.todo('ship isSunk() function checks if ship is sunk');
+  expect(() => {
+    tryErrorShip();
+  }).toThrow();
+});
+
+it('ship hit() function increase number of hits', () => {
+  const ship = new Ship(3);
+  expect(ship.hitsCount).toBe(0);
+  ship.hit();
+  expect(ship.hitsCount).toBe(1);
+});
+
+it('ship isSunk() function checks if ship is sunk', () => {
+  const ship = new Ship(1);
+  expect(ship.isSunk()).toBe(false);
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
