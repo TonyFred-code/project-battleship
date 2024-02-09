4434fbe23ca43e9bca2d3df5824a69bb4f66d512
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-02-09 15:01:18 +0100

    feat: remove test code used in checking jest checkup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Ship from './ship';
-import evaluateGuess from './evaluate-guess';
 
 const hello = () => console.log('hello');
 
@@ -12,16 +11,3 @@ console.log(spread);
 hello();
 
 console.log(new Ship(2));
-
-const MAGIC_NUMBER = Math.floor(Math.random() * 10);
-
-function runGame() {
-  const output = document.querySelector('output');
-  const input = document.querySelector('input');
-  const { value } = input;
-
-  output.textContent = evaluateGuess(MAGIC_NUMBER, Number(value));
-}
-
-const submitBtn = document.querySelector('.submit');
-submitBtn.addEventListener('click', runGame);
