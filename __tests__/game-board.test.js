20665440bcdc27faf6546b3367d1395144a752ce
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-02-10 14:29:48 +0100

    feat: add tests for receiveAttack method

--- __tests__/game-board.test.js
+++ __tests__/game-board.test.js
@@ -49,11 +49,62 @@ it('game should refuse to place ship on grids by the side of a ship on the board
   expect(gameBoard.placeShip(2, 3, 3, 'vertical')).toBe(false);
 });
 
-it.todo('gameBoard should have a receiveAttack() method');
+it('gameBoard should have a receiveAttack() method', () => {
+  const gameBoard = new GameBoard();
+  expect(gameBoard.receiveAttack).not.toBeUndefined();
+});
 
-it.todo('receiveAttack() should determine if a hit is to a ship or not');
+it('receiveAttack should refuse hit if no ship is on the board', () => {
+  const gameBoard = new GameBoard();
+  expect(gameBoard.receiveAttack(1, 3)).toBe(false);
+});
 
-it.todo('gameBoard should report when all ships are sunk');
+it('gameBoard receiveAttack should not hit off the board', () => {
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 1, 3);
+  expect(gameBoard.receiveAttack(1, 10)).toBe(false);
+});
+
+it('gameBoard receiveAttack should hit a ship on the board', () => {
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 1, 3);
+  expect(gameBoard.receiveAttack(1, 3)).toBe(true);
+});
+
+it('gameBoard receiveAttack should hit a unoccupied node on the board', () => {
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 1, 3);
+  expect(gameBoard.receiveAttack(9, 3)).toBe(true);
+});
+
+it('gameBoard receiveAttack should not hit same spot twice', () => {
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 1, 3);
+  expect(gameBoard.receiveAttack(1, 3)).toBe(true);
+  expect(gameBoard.receiveAttack(1, 3)).toBe(false);
+});
+
+it('gameBoard receiveAttack should deny hit when all ship is sunk', () => {
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(1, 1, 3);
+  expect(gameBoard.receiveAttack(1, 3)).toBe(true);
+  expect(gameBoard.receiveAttack(9, 9)).toBe(false);
+});
+
+it('gameBoard receiveAttack should deny hit when all ship is sunk 2', () => {
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 2, 8);
+  expect(gameBoard.receiveAttack(2, 8)).toBe(true);
+  expect(gameBoard.receiveAttack(3, 8)).toBe(true);
+  expect(gameBoard.receiveAttack(9, 9)).toBe(false);
+});
+
+it('gameBoard should report when all ships are sunk', () => {
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(1, 1, 3);
+  gameBoard.receiveAttack(1, 3);
+  expect(gameBoard.isAllShipSunk).toBe(true);
+});
 
 /*
 
