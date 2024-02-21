f01b8b2222be810ce460e6effd6020f15946a25d
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-02-21 15:11:16 +0100

    feat: add methods required by specs to use Player
    add ship subclass placing methods
    add method for receiving attack
    add method for getting board state

--- src/player.js
+++ src/player.js
@@ -1,1 +1,47 @@
-export default class Player {}
+import GameBoard from "./game-board";
+
+export default class Player {
+
+  #PLAYER_BOARD = new GameBoard();
+
+  constructor(name) {
+    if (!this.#isValidString(name)) {
+      throw new Error('Invalid name parameter');
+    }
+
+    this.name = name;
+  }
+
+  #isValidString(string) {
+    return typeof string === 'string' && string.trim() !== '';
+  }
+
+  getBoard() {
+    return this.#PLAYER_BOARD;
+  }
+
+  placeCarrier(x, y, orientation = 'horizontal') {
+    return this.#PLAYER_BOARD.placeCarrier(x, y, orientation);
+  }
+
+  placeBattleShip(x, y, orientation = 'horizontal') {
+    return this.#PLAYER_BOARD.placeBattleShip(x, y, orientation);
+  }
+
+  placeDestroyer(x, y, orientation = 'horizontal') {
+    return this.#PLAYER_BOARD.placeDestroyer(x, y, orientation);
+  }
+
+  placeSubMarine(x, y, orientation = 'horizontal') {
+    return this.#PLAYER_BOARD.placeSubMarine(x, y, orientation)
+  }
+
+  placePatrolBoat(x, y, orientation = 'horizontal') {
+    return this.#PLAYER_BOARD.placePatrolBoat(x, y, orientation);
+  }
+
+  receiveAttack(x, y) {
+    const status = this.#PLAYER_BOARD.receiveAttack(x, y);
+    return status;
+  }
+}
