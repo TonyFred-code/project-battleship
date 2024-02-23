ef5cb348ff3a68cceda74745e94e8f2ac987adfd
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-02-23 13:11:39 +0100

    feat: complete getAttack to return proper valid coordinates

--- src/computer-player.js
+++ src/computer-player.js
@@ -11,15 +11,17 @@ export default class ComputerPlayer extends Player {
     if (validMoves.length === 0) return [];
 
     function getRnd(array) {
-      const max = array.length;
+      const max = array.length - 1;
       const min = 0;
       const pos = Math.floor(Math.random() * (max - min + 1) + min);
 
       return pos;
     }
 
-    const attackNode = getRnd(validMoves);
+    const pos = getRnd(validMoves);
+    const node = validMoves[pos];
+    const { address } = node;
 
-    return validMoves[attackNode].address;
+    return address;
   }
 }
