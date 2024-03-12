97ddfe06db49bcc3e18e2087dc5f6f202cf58d1b
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-03-12 04:31:20 +0100

    feat: create test suites for destroyerAutoPlace

--- __tests__/game-board.test.js
+++ __tests__/game-board.test.js
@@ -1307,6 +1307,129 @@ describe('auto ship placement', () => {
 
       expect(destroyerPlacements).toHaveLength(0);
     });
+
+    test('destroyerAutoPlace exists', () => {
+      expect(gameBoard.destroyerAutoPlace).toBeDefined();
+    });
+
+    test('destroyerAutoPlace returns obj following a pattern', () => {
+      const placeAttribute = gameBoard.destroyerAutoPlace('horizontal');
+
+      expect(placeAttribute).toHaveProperty('orientation');
+
+      const { orientation } = placeAttribute;
+
+      expect(orientation === 'horizontal').toBe(true);
+
+      expect(placeAttribute).toHaveProperty('placeHead');
+
+      const HorizontalPlacements = [
+        '0-0',
+        '1-0',
+        '2-0',
+        '3-0',
+        '4-0',
+        '5-0',
+        '6-0',
+        '7-0',
+        '0-1',
+        '1-1',
+        '2-1',
+        '3-1',
+        '4-1',
+        '5-1',
+        '6-1',
+        '7-1',
+        '0-2',
+        '1-2',
+        '2-2',
+        '3-2',
+        '4-2',
+        '5-2',
+        '6-2',
+        '7-2',
+        '0-3',
+        '1-3',
+        '2-3',
+        '3-3',
+        '4-3',
+        '5-3',
+        '6-3',
+        '7-3',
+        '0-4',
+        '1-4',
+        '2-4',
+        '3-4',
+        '4-4',
+        '5-4',
+        '6-4',
+        '7-4',
+        '0-5',
+        '1-5',
+        '2-5',
+        '3-5',
+        '4-5',
+        '5-5',
+        '6-5',
+        '7-5',
+        '0-6',
+        '1-6',
+        '2-6',
+        '3-6',
+        '4-6',
+        '5-6',
+        '6-6',
+        '7-6',
+        '0-7',
+        '1-7',
+        '2-7',
+        '3-7',
+        '4-7',
+        '5-7',
+        '6-7',
+        '7-7',
+        '0-8',
+        '1-8',
+        '2-8',
+        '3-8',
+        '4-8',
+        '5-8',
+        '6-8',
+        '7-8',
+        '0-9',
+        '1-9',
+        '2-9',
+        '3-9',
+        '4-9',
+        '5-9',
+        '6-9',
+        '7-9',
+      ];
+
+      const { placeHead } = placeAttribute;
+
+      const formattedPlaceHead = `${placeHead[0]}-${placeHead[1]}`;
+
+      expect(HorizontalPlacements.includes(formattedPlaceHead)).toBe(true);
+
+      expect(placeAttribute).toHaveProperty('occupyingNodeLoc');
+    });
+
+    test('destroyerAutoPlace can place vertically', () => {
+      const placeAttribute = gameBoard.destroyerAutoPlace('vertical');
+
+      const { orientation } = placeAttribute;
+
+      expect(orientation === 'vertical').toBe(true);
+    });
+
+    test('destroyerAutoPlace refuse invalid orientation', () => {
+      const placeAttribute = gameBoard.destroyerAutoPlace('something');
+
+      const keys = Object.keys(placeAttribute);
+
+      expect(keys).toHaveLength(0);
+    });
   });
 
   describe('submarine auto placement', () => {
