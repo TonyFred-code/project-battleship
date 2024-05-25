e4f98ae645d6c3fe2f4e77984f168af9c919cad3
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-05-25 08:35:07 +0100

    feat: add test for roundState method
    remove test on ship particulars

--- __tests__/game-round.test.js
+++ __tests__/game-round.test.js
@@ -93,7 +93,113 @@ describe('can add human and bot players', () => {
   // });
 });
 
-describe('round state can be returned', () => {});
+describe('round state can be returned', () => {
+  const gameRound = new GameRound();
+
+  test('should have roundState()', () => {
+    expect(gameRound.roundState).toBeDefined();
+  });
+
+  test('roundState returns proper round particulars', () => {
+    let { roundState } = gameRound;
+
+    expect(roundState).toHaveProperty('roundWon');
+    expect(roundState).toHaveProperty('winnerName');
+
+    gameRound.addBotPlayer();
+    gameRound.addHumanPlayer('Player 2');
+
+    gameRound.autoPlaceBotShips();
+    gameRound.autoPlaceHumanShips();
+
+    const botShipConfig = gameRound.botShipDetails();
+    // const humanShipConfig = ;
+
+    // use ship config to make a player win and check
+    // to ensure roundState() reflects the round win
+
+    const botCarrier = botShipConfig.carrierPlacement;
+    const botBattleShip = botShipConfig.battleShipPlacement;
+    const botDestroyer = botShipConfig.destroyerPlacement;
+    const botSubMarine = botShipConfig.subMarinePlacement;
+    const botPatrolBoat = botShipConfig.patrolBoatPlacement;
+
+    // const humanCarrier = humanShipConfig.carrierPlacement;
+    // const humanBattleShip = humanShipConfig.battleShipPlacement;
+    // const humanDestroyer = humanShipConfig.destroyerPlacement;
+    // const humanSubMarine = humanShipConfig.subMarinePlacement;
+    // const humanPatrolBoat = humanShipConfig.patrolBoatPlacement;
+
+    const botCarrierShipConfig = botCarrier.occupyingLoc;
+    const botBattleShipShipConfig = botBattleShip.occupyingLoc;
+    const botDestroyerShipConfig = botDestroyer.occupyingLoc;
+    const botSubMarineShipConfig = botSubMarine.occupyingLoc;
+    const botPatrolBoatShipConfig = botPatrolBoat.occupyingLoc;
+
+    // hit ships
+    function hit(locationNodes) {
+      const statusCodes = new Set();
+
+      locationNodes.forEach((locationNode) => {
+        const [x, y] = locationNode;
+        const statusCode = gameRound.humanPlayerMove(x, y);
+        statusCodes.add(statusCode);
+      });
+
+      return [...statusCodes];
+    }
+
+    const botCarrierHitStatus = hit(botCarrierShipConfig);
+
+    expect(botCarrierHitStatus).toHaveLength(1);
+    expect(botCarrierHitStatus.includes(1)).toBe(true);
+
+    ({ roundState } = gameRound);
+
+    expect(roundState.roundWon).toBeFalsy();
+    expect(roundState.winnerName).toBe('');
+
+    const botBattleShipHitStatus = hit(botBattleShipShipConfig);
+
+    expect(botBattleShipHitStatus).toHaveLength(1);
+    expect(botBattleShipHitStatus.includes(1)).toBe(true);
+
+    ({ roundState } = gameRound);
+
+    expect(roundState.roundWon).toBeFalsy();
+    expect(roundState.winnerName).toBe('');
+
+    const botDestroyerHitStatus = hit(botDestroyerShipConfig);
+
+    expect(botDestroyerHitStatus).toHaveLength(1);
+    expect(botDestroyerHitStatus.includes(1)).toBe(true);
+
+    ({ roundState } = gameRound);
+
+    expect(roundState.roundWon).toBeFalsy();
+    expect(roundState.winnerName).toBe('');
+
+    const botSubMarineHitStatus = hit(botSubMarineShipConfig);
+
+    expect(botSubMarineHitStatus).toHaveLength(1);
+    expect(botSubMarineHitStatus.includes(1)).toBe(true);
+
+    ({ roundState } = gameRound);
+
+    expect(roundState.roundWon).toBeFalsy();
+    expect(roundState.winnerName).toBe('');
+
+    const botPatrolBoatHItStatus = hit(botPatrolBoatShipConfig);
+
+    expect(botPatrolBoatHItStatus).toHaveLength(1);
+    expect(botPatrolBoatHItStatus.includes(1)).toBe(true);
+
+    ({ roundState } = gameRound);
+
+    expect(roundState.roundWon).toBeTruthy();
+    expect(roundState.winnerName).toBe('Player 2');
+  });
+});
 
 describe('can return particulars of both players', () => {
   test('humanPlayerDetails method exists', () => {
@@ -194,45 +300,47 @@ describe('allows playing in turns', () => {
   });
 });
 
-describe('can return ship particulars of both players', () => {
-  const gameRound = new GameRound();
+// describe('can return ship particulars of both players', () => {
+//   const gameRound = new GameRound();
 
-  gameRound.addBotPlayer();
+//   gameRound.addBotPlayer();
 
-  gameRound.addHumanPlayer('Player 2');
-  test('botShipDetails method exists', () => {
-    expect(gameRound.botShipDetails).toBeDefined();
-  });
+//   gameRound.addHumanPlayer('Player 2');
+//   test('botShipDetails method exists', () => {
+//     expect(gameRound.botShipDetails).toBeDefined();
+//   });
 
-  test('humanPlayerShipDetails method exists', () => {
-    expect(gameRound.humanPlayerShipDetails).toBeDefined();
-  });
+//   test('humanPlayerShipDetails method exists', () => {
+//     expect(gameRound.humanPlayerShipDetails).toBeDefined();
+//   });
 
-  test('placeBotShips method exists', () => {
-    expect(gameRound.autoPlaceBotShips).toBeDefined();
-  });
+//   test('placeBotShips method exists', () => {
+//     expect(gameRound.autoPlaceBotShips).toBeDefined();
+//   });
 
-  describe('human ship placement methods', () => {
-    test('human ship placing methods exists', () => {
-      expect(gameRound.placeHumanPlayerCarrier).toBeDefined();
+//   describe('human ship placement methods', () => {
+//     test('human ship placing methods exists', () => {
+//       expect(gameRound.placeHumanPlayerCarrier).toBeDefined();
 
-      expect(gameRound.placeHumanPlayerBattleShip).toBeDefined();
+//       expect(gameRound.placeHumanPlayerBattleShip).toBeDefined();
 
-      expect(gameRound.placeHumanPlayerDestroyer).toBeDefined();
+//       expect(gameRound.placeHumanPlayerDestroyer).toBeDefined();
 
-      expect(gameRound.placeHumanPlayerSubMarine).toBeDefined();
+//       expect(gameRound.placeHumanPlayerSubMarine).toBeDefined();
 
-      expect(gameRound.placeHumanPlayerPatrolBoat).toBeDefined();
+//       expect(gameRound.placeHumanPlayerPatrolBoat).toBeDefined();
 
-      expect(gameRound.autoPlaceHumanShips).toBeDefined();
-    });
-  });
+//       expect(gameRound.autoPlaceHumanShips).toBeDefined();
+//     });
+//   });
 
-  test('botShipDetails returns correct data', () => {
-    // expect(gameRound.botShipDetails()).toBeFalsy();
+//   test('botShipDetails returns correct data', () => {
+//     // expect(gameRound.botShipDetails()).toBeFalsy();
 
-    gameRound.autoPlaceBotShips();
+//     gameRound.autoPlaceBotShips();
 
-    expect(gameRound.botShipDetails()).toBeTruthy();
-  });
-});
+//     expect(gameRound.botShipDetails()).toBeTruthy();
+//   });
+// });
+
+// todo: add test to properly check for continuous hits
