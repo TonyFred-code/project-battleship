0971bc28e1e5739e22a850d1ab87bcf61410aa4a
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-05-30 13:41:21 +0100

    feat: add methods for playing a round to end state

--- src/game-controller.js
+++ src/game-controller.js
@@ -1,174 +1,104 @@
-import HumanPlayer from './human-player.js';
-import ComputerPlayer from './computer-player.js';
+import GameRound from './game-round.js';
 
 export default class GameController {
-  #HUMAN_PLAYER = null;
-
-  #BOT_PLAYER = null;
-
-  #BOT_DETAILS = {
-    initialized: false,
-    placedShips: false,
-  };
-
-  #HUMAN_PLAYER_DETAILS = {
-    initialized: false,
-    carrierPlaced: false,
-    battleShipPlaced: false,
-    destroyerPlaced: false,
-    subMarinePlaced: false,
-    patrolBoatPlaced: false,
-    allShipsPlaced: false,
-    markAllPlaced() {
-      this.carrierPlaced = true;
-      this.battleShipPlaced = true;
-      this.destroyerPlaced = true;
-      this.subMarinePlaced = true;
-      this.patrolBoatPlaced = true;
-      this.allShipsPlaced = true;
-    },
-    placedShips() {
-      return (
-        this.carrierPlaced &&
-        this.battleShipPlaced &&
-        this.destroyerPlaced &&
-        this.subMarinePlaced &&
-        this.patrolBoatPlaced
-      );
-    },
-  };
+  #GAME_ROUND = null;
 
   #GAME_START = false;
 
   #GAME_END = false;
 
   createHumanPlayer(playerName) {
-    if (typeof playerName !== 'string' || playerName.trim() === '') {
-      return false;
-    }
+    if (!this.#GAME_START) return false;
 
-    if (this.#HUMAN_PLAYER === null) {
-      this.#HUMAN_PLAYER = new HumanPlayer(playerName);
-      this.#HUMAN_PLAYER_DETAILS.initialized = true;
-      return true;
-    }
-
-    return false;
+    return this.#GAME_ROUND.addHumanPlayer(playerName);
   }
 
   createBotPlayer() {
-    if (this.#BOT_PLAYER === null) {
-      this.#BOT_PLAYER = new ComputerPlayer();
-      this.#BOT_DETAILS.initialized = true;
+    if (!this.#GAME_START) return false;
 
-      this.placeBotShips();
-      this.#BOT_DETAILS.placedShips = true;
-
-      return true;
-    }
-
-    return false;
+    return this.#GAME_ROUND.addBotPlayer();
   }
 
   placeHumanPlayerCarrier(x, y, orientation) {
-    const state = this.#HUMAN_PLAYER.placeCarrier(x, y, orientation);
+    if (!this.#GAME_START) return false;
 
-    if (state) {
-      this.#HUMAN_PLAYER_DETAILS.carrierPlaced = true;
-    }
-    return state;
+    return this.#GAME_ROUND.placeHumanPlayerCarrier(x, y, orientation);
   }
 
   placeHumanPlayerBattleShip(x, y, orientation) {
-    const state = this.#HUMAN_PLAYER.placeBattleShip(x, y, orientation);
+    if (!this.#GAME_START) return false;
 
-    if (state) {
-      this.#HUMAN_PLAYER_DETAILS.battleShipPlaced = true;
-    }
-    return state;
+    return this.#GAME_ROUND.placeHumanPlayerBattleShip(x, y, orientation);
   }
 
   placeHumanPlayerDestroyer(x, y, orientation) {
-    const state = this.#HUMAN_PLAYER.placeDestroyer(x, y, orientation);
+    if (!this.#GAME_START) return false;
 
-    if (state) {
-      this.#HUMAN_PLAYER_DETAILS.destroyerPlaced = true;
-    }
-    return state;
+    return this.#GAME_ROUND.placeHumanPlayerDestroyer(x, y, orientation);
   }
 
   placeHumanPlayerSubMarine(x, y, orientation) {
-    const state = this.#HUMAN_PLAYER.placeSubMarine(x, y, orientation);
+    if (!this.#GAME_START) return false;
 
-    if (state) {
-      this.#HUMAN_PLAYER_DETAILS.subMarinePlaced = true;
-    }
-    return state;
+    return this.#GAME_ROUND.placeHumanPlayerSubMarine(x, y, orientation);
   }
 
   placeHumanPlayerPatrolBoat(x, y, orientation) {
-    const state = this.#HUMAN_PLAYER.placePatrolBoat(x, y, orientation);
+    if (!this.#GAME_START) return false;
 
-    if (state) {
-      this.#HUMAN_PLAYER_DETAILS.patrolBoatPlaced = true;
-    }
-    return state;
+    return this.#GAME_ROUND.placeHumanPlayerPatrolBoat(x, y, orientation);
   }
 
   autoPlaceHumanPlayerShips() {
-    const state = this.#HUMAN_PLAYER.autoPlaceShips();
+    if (!this.#GAME_START) return false;
 
-    if (state) {
-      this.#HUMAN_PLAYER_DETAILS.markAllPlaced();
-    }
-
-    return state;
+    return this.#GAME_ROUND.autoPlaceHumanShips();
   }
 
-  placeBotShips() {
-    return this.#BOT_PLAYER.autoPlaceShips();
+  autoPlaceBotShips() {
+    if (!this.#GAME_START) return false;
+
+    return this.#GAME_ROUND.autoPlaceBotShips();
   }
 
-  getPlayerDetails() {
-    const playerDetails = [];
+  get humanPlayerDetails() {
+    if (!this.#GAME_START) return false;
 
-    if (this.#HUMAN_PLAYER) {
-      const player1 = {
-        name: this.#HUMAN_PLAYER.name,
-      };
-      playerDetails.push(player1);
-    }
-
-    if (this.#BOT_PLAYER) {
-      const player2 = {
-        name: this.#BOT_PLAYER.name,
-      };
-      playerDetails.push(player2);
-    }
-
-    return playerDetails;
+    return this.#GAME_ROUND.humanPlayerDetails();
   }
 
-  #playersComplete() {
-    return this.#BOT_PLAYER !== null && this.#HUMAN_PLAYER !== null;
+  get botPlayerDetails() {
+    if (!this.#GAME_START) return false;
+
+    return this.#GAME_ROUND.botPlayerDetails();
+  }
+
+  humanPlayerShipDetails() {
+    if (!this.#GAME_START) return {};
+
+    return this.#GAME_ROUND.humanPlayerShipDetails();
+  }
+
+  botPlayerShipDetails() {
+    if (!this.#GAME_START) return {};
+
+    return this.#GAME_ROUND.botPlayerShipDetails();
   }
 
   get gameState() {
-    // winner name
+    // winner name?
     // lost or won
     // game ended
     const GAME_STATE = {
-      // humanPlayerName,
-      // botPlayerName,
       // roundStart,
       // roundEnd,
     };
 
-    GAME_STATE.humanPlayerName = this.#HUMAN_PLAYER
-      ? this.#HUMAN_PLAYER.name
-      : null;
+    // GAME_STATE.humanPlayerName = this.#HUMAN_PLAYER
+    //   ? this.#HUMAN_PLAYER.name
+    //   : null;
 
-    GAME_STATE.botPlayerName = this.#BOT_PLAYER ? this.#BOT_PLAYER.name : null;
+    // GAME_STATE.botPlayerName = this.#BOT_PLAYER ? this.#BOT_PLAYER.name : null;
 
     GAME_STATE.roundStart = this.#GAME_START;
 
@@ -177,25 +107,149 @@ export default class GameController {
     return GAME_STATE;
   }
 
+  get roundState() {
+    if (this.#GAME_ROUND === null) return {};
+
+    return this.#GAME_ROUND.roundState;
+  }
+
+  getActivePlayer() {
+    if (!this.#GAME_START) return {};
+
+    return this.#GAME_ROUND.getActivePlayer();
+  }
+
+  botMove(x, y) {
+    if (!this.#GAME_START) return false;
+
+    const status = this.#GAME_ROUND.botMove(x, y);
+
+    const { roundWon } = this.roundState;
+
+    if (roundWon) {
+      this.endRound();
+    }
+
+    return status;
+  }
+
+  humanPlayerMove(x, y) {
+    if (!this.#GAME_START) return false;
+
+    const status = this.#GAME_ROUND.humanPlayerMove(x, y);
+
+    const { roundWon } = this.roundState;
+
+    if (roundWon) {
+      this.endRound();
+    }
+
+    return status;
+  }
+
   restartRound() {}
 
   endRound() {
-    // this = new GameController();
+    this.#GAME_END = true;
+    this.#GAME_START = false;
   }
 
   startRound() {
-    if (
-      this.#BOT_DETAILS.initialized &&
-      this.#HUMAN_PLAYER_DETAILS.initialized &&
-      this.#BOT_DETAILS.placedShips &&
-      this.#HUMAN_PLAYER_DETAILS.placedShips()
-    ) {
-      this.#GAME_START = true;
-      this.#GAME_END = false;
-    }
+    this.#GAME_ROUND = new GameRound();
+    this.#GAME_START = true;
+    this.#GAME_END = false;
   }
-
-  get botPlayerDetails() {}
-
-  get humanPlayerDetails() {}
 }
+
+// const gameController = new GameController();
+
+// gameController.startRound();
+
+// gameController.createHumanPlayer('Player 1');
+// gameController.createBotPlayer();
+
+// // const { roundState } = gameController;s
+
+// // Individual ship placements
+
+// gameController.placeHumanPlayerCarrier(5, 0);
+
+// gameController.placeHumanPlayerBattleShip(0, 4, 'vertical');
+
+// gameController.placeHumanPlayerDestroyer(3, 3);
+// gameController.placeHumanPlayerSubMarine(6, 6, 'vertical');
+
+// gameController.placeHumanPlayerPatrolBoat(1, 9);
+
+// const { roundStart, roundEnd } = gameController.gameState;
+
+// console.table(gameController.gameState);
+
+// const activePlayer = gameController.getActivePlayer();
+
+// console.table(activePlayer);
+
+// const humanPlayerShipConfig = gameController.humanPlayerShipDetails();
+
+// // const botCarrier = botShipConfig.carrierPlacement;
+// // const botBattleShip = botShipConfig.battleShipPlacement;
+// // const botDestroyer = botShipConfig.destroyerPlacement;
+// // const botSubMarine = botShipConfig.subMarinePlacement;
+// // const botPatrolBoat = botShipConfig.patrolBoatPlacement;
+
+// const humanPlayerCarrier = humanPlayerShipConfig.carrierPlacement;
+// const humanPlayerBattleShip = humanPlayerShipConfig.battleShipPlacement;
+// const humanPlayerDestroyer = humanPlayerShipConfig.destroyerPlacement;
+// const humanPlayerSubMarine = humanPlayerShipConfig.subMarinePlacement;
+// const humanPlayerPatrolBoat = humanPlayerShipConfig.patrolBoatPlacement;
+
+// // const botCarrierShipConfig = botCarrier.occupyingLoc;
+// // const botBattleShipShipConfig = botBattleShip.occupyingLoc;
+// // const botDestroyerShipConfig = botDestroyer.occupyingLoc;
+// // const botSubMarineShipConfig = botSubMarine.occupyingLoc;
+// // const botPatrolBoatShipConfig = botPatrolBoat.occupyingLoc;
+
+// const humanPlayerCarrierShipConfig = humanPlayerCarrier.occupyingLoc;
+// const humanPlayerBattleShipShipConfig = humanPlayerBattleShip.occupyingLoc;
+// const humanPlayerDestroyerShipConfig = humanPlayerDestroyer.occupyingLoc;
+// const humanPlayerSubMarineShipConfig = humanPlayerSubMarine.occupyingLoc;
+// const humanPlayerPatrolBoatShipConfig = humanPlayerPatrolBoat.occupyingLoc;
+
+// // function humanHit(locationNodes) {
+// //   const statusCodes = new Set();
+
+// //   locationNodes.forEach((locationNode) => {
+// //     const [x, y] = locationNode;
+// //     const statusCode = gameRound.humanPlayerMove(x, y);
+// //     statusCodes.add(statusCode);
+// //   });
+
+// //   return [...statusCodes];
+// // }
+
+// function botHit(locationNodes) {
+//   const statusCodes = new Set();
+
+//   locationNodes.forEach((locationNode) => {
+//     const [x, y] = locationNode;
+//     const statusCode = gameController.botMove(x, y);
+//     statusCodes.add(statusCode);
+//   });
+
+//   return [...statusCodes];
+// }
+
+// // auto sink all ships
+// botHit(humanPlayerCarrierShipConfig);
+// botHit(humanPlayerBattleShipShipConfig);
+// botHit(humanPlayerDestroyerShipConfig);
+// botHit(humanPlayerSubMarineShipConfig);
+// botHit(humanPlayerPatrolBoatShipConfig);
+
+// // ({ roundEnd, roundStart } = gameController.gameState);
+
+// console.log(gameController.roundState);
+
+// const { roundState } = gameController;
+
+// console.log(roundState);
