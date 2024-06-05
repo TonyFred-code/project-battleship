a5a2421ddb8a0186b1ffc4b16fd52bb412d0bc67
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-06-05 08:14:50 +0100

    feat: import create loading screen module

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Ship from './ship.js';
+// import './dom_module/loading-screen.js';
 import './style.css';
+import createLoadScreen from './dom_module/loading-screen.js';
 
+const content = document.querySelector('body');
+
+const loadingScreenContainer = createLoadScreen();
+content.appendChild(loadingScreenContainer);
 const hello = () => console.log('hello');
 
 const array = ['a', 'b', 'c'];
