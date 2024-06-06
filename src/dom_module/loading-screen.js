8f8edb7a496800a93a23838af36b9c8231866c53
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-06-06 13:05:18 +0100

    feat: add line as part of returned value

--- src/dom_module/loading-screen.js
+++ src/dom_module/loading-screen.js
@@ -63,5 +63,5 @@ export default function createLoadScreen() {
 
   loadingScreenContainer.appendChild(genericContainer);
 
-  return loadingScreenContainer;
+  return { loadingScreenContainer, line };
 }
