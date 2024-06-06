e35a2c2dba8784cb165826779c950192eb6efa49
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-06-06 12:00:56 +0100

    feat: import createElementWithClass fnc

--- src/dom_module/loading-screen.js
+++ src/dom_module/loading-screen.js
@@ -1,16 +1,4 @@
-function createElementWithClass(element, classNames) {
-  const ELEMENT = document.createElement(element);
-
-  if (classNames && classNames.length !== 0) {
-    classNames.forEach((className) => {
-      if (className) {
-        ELEMENT.classList.add(className);
-      }
-    });
-  }
-
-  return ELEMENT;
-}
+import createElementWithClass from '../helper_module/create-element-with-class.js';
 
 export default function createLoadScreen() {
   const loadingScreenContainer = createElementWithClass('div', [
