3364d12e8c0d450d43abcfab9a105f50b8c5fb6f
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-10-08 00:04:46 +0100

    feat: add name property
    feat: update hits return value

--- src/ship.js
+++ src/ship.js
@@ -5,21 +5,26 @@ export default class Ship {
     return length > 0;
   }
 
-  constructor(length) {
+  constructor(length, name) {
     if (!Ship.isValidLength(length)) {
       throw new Error('Invalid length. 1 is minimum ship length');
     }
 
     this.length = length;
     this.hitsCount = 0;
+    this.name = name;
   }
 
   hit() {
-    if (this.#isSunk) return false;
+    if (this.#isSunk) return -1;
 
     this.hitsCount += 1;
-    if (this.isSunk()) this.#isSunk = true;
-    return true;
+    if (this.isSunk()) {
+      this.#isSunk = true;
+      return 2;
+    }
+
+    return 1;
   }
 
   isSunk() {
