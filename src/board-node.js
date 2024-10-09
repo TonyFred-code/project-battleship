19949d8538de959c1fdb33e6e37fd0736eff4519
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-10-09 14:37:49 +0100

    remove commented out code

--- src/board-node.js
+++ src/board-node.js
@@ -1,17 +1,3 @@
-// export default class Node {
-//   constructor(x, y) {
-//     this.address = [x, y];
-//     this.neighbors = [];
-//     this.isHit = false;
-//     this.isOccupied = false;
-//     this.isNeighboringOccupied = false;
-//   }
-
-//   get neighboringOccupied() {
-//     return this.neighbors.every((neighbor) => !neighbor.isOccupied);
-//   }
-// }
-
 import Ship from './ship.js';
 
 export default class Node {
