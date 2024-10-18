6881bc1001f27f8dbe3f65903745e179b5250953
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-10-18 22:52:46 +0100

    feat: add methods to meet with test criteria
    remove public obj properties
    add methods for accessing object properties

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,12 @@
 export default class Ship {
   #isSunk = false;
 
+  #size;
+
+  #hitsCount = 0;
+
+  #name = '';
+
   static isValidLength(length) {
     return length > 0;
   }
@@ -10,15 +16,14 @@ export default class Ship {
       throw new Error('Invalid length. 1 is minimum ship length');
     }
 
-    this.length = length;
-    this.hitsCount = 0;
-    this.name = name;
+    this.#size = length;
+    this.#name = name;
   }
 
   hit() {
-    if (this.#isSunk) return -1;
+    if (this.isSunk()) return -1;
 
-    this.hitsCount += 1;
+    this.#hitsCount += 1;
     if (this.isSunk()) {
       this.#isSunk = true;
       return 2;
@@ -27,7 +32,19 @@ export default class Ship {
     return 1;
   }
 
+  get name() {
+    return this.#name;
+  }
+
+  get hitsCount() {
+    return this.#hitsCount;
+  }
+
+  get size() {
+    return this.#size;
+  }
+
   isSunk() {
-    return this.hitsCount === this.length;
+    return this.#hitsCount === this.#size;
   }
 }
