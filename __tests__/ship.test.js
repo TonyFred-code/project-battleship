1697eda90b939df9a844ca14b678a94bab5e718c
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-10-20 17:37:31 +0100

    feat: add test suites
    test for place origin assignment and removal

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -4,23 +4,24 @@ describe('Ship', () => {
   let ship;
 
   beforeEach(() => {
-    ship = new Ship(1, 'test');
+    ship = new Ship(2, 'test');
   });
 
   describe('hit', () => {
-    test('should return -1 if ship is has been sunk', () => {
+    test('should return -1 if ship has been sunk', () => {
+      ship.hit();
       ship.hit();
 
       expect(ship.hit()).toBe(-1);
     });
 
     test('should return 1 if ship is hit without sinking', () => {
-      const ship1 = new Ship(2, 'test');
-
-      expect(ship1.hit()).toBe(1);
+      expect(ship.hit()).toBe(1);
     });
 
     test('should return 2 if hit causes ship to be sunk', () => {
+      ship.hit();
+
       expect(ship.hit()).toBe(2);
     });
   });
@@ -43,7 +44,7 @@ describe('Ship', () => {
 
   describe('size', () => {
     test('should return size of ship', () => {
-      expect(ship.size).toBe(1);
+      expect(ship.size).toBe(2);
     });
   });
 
@@ -54,8 +55,78 @@ describe('Ship', () => {
 
     test('should return true if ship has been sunk', () => {
       ship.hit();
+      ship.hit();
 
       expect(ship.isSunk()).toBe(true);
+    });
+  });
+
+  describe('assignPlaceOrigin', () => {
+    test('should throw error if x coordinate is negative', () => {
+      expect(() => {
+        ship.assignPlaceOrigin(-1, 0);
+      }).toThrow(/invalid/i);
+    });
+
+    test('should throw error if x coordinate is non-numeric', () => {
+      expect(() => {
+        ship.assignPlaceOrigin('1', 0);
+      }).toThrow(/invalid/i);
+    });
+
+    test('should throw error if y coordinate is negative', () => {
+      expect(() => {
+        ship.assignPlaceOrigin(0, -1);
+      }).toThrow(/invalid/i);
+    });
+
+    test('should throw error if y coordinate is non-numeric', () => {
+      expect(() => {
+        ship.assignPlaceOrigin(0, '1');
+      }).toThrow(/invalid/i);
+    });
+
+    test('should throw error if ship has been placed', () => {
+      ship.assignPlaceOrigin(0, 1);
+
+      expect(() => {
+        ship.assignPlaceOrigin(0, 0);
+      }).toThrow();
+    });
+
+    test('should assign place origin', () => {
+      ship.assignPlaceOrigin(0, 1);
+
+      const { x, y } = ship.assignedPlaceOrigin;
+
+      expect(x).toBe(0);
+      expect(y).toBe(1);
+    });
+  });
+
+  describe('assignedPlaceOrigin', () => {
+    test('should return place origin assigned to ship', () => {
+      expect(ship.assignedPlaceOrigin).toEqual({ x: null, y: null });
+
+      ship.assignPlaceOrigin(0, 1);
+
+      expect(ship.assignedPlaceOrigin).toEqual({ x: 0, y: 1 });
+    });
+  });
+
+  describe('removeAssignedPlaceOrigin', () => {
+    test('should remove previously assigned place origin', () => {
+      ship.assignPlaceOrigin(0, 0);
+
+      ship.removeAssignedPlaceOrigin();
+
+      expect(ship.assignedPlaceOrigin).toEqual({ x: null, y: null });
+    });
+
+    test('should run fine with an attempt to remove before being placed', () => {
+      ship.removeAssignedPlaceOrigin();
+
+      expect(ship.assignedPlaceOrigin).toEqual({ x: null, y: null });
     });
   });
 });
