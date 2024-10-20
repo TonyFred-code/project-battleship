4716dd6e2f0164de9327171bd1f8ec22718f56f9
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-10-20 04:13:24 +0100

    feat: reconfigure test suite
    test for only proper class initialization parameters

--- __tests__/submarine.test.js
+++ __tests__/submarine.test.js
@@ -1,32 +1,14 @@
 import SubMarine from '../src/submarine.js';
-import Ship from '../src/ship.js';
 
-test('can create a submarine class', () => {
-  expect(new SubMarine()).not.toBeUndefined();
-});
+describe('SubMarine class', () => {
+  let submarine;
 
-test('is a ship extension', () => {
-  const submarine = new SubMarine();
-  expect(submarine instanceof Ship).toBe(true);
-});
+  beforeEach(() => {
+    submarine = new SubMarine();
+  });
 
-test('has a fixed length of 3', () => {
-  const submarine = new SubMarine(6);
-  expect(submarine).toHaveLength(3);
-});
-
-test('is sunk in 3 hits', () => {
-  const submarine = new SubMarine();
-  submarine.hit();
-  submarine.hit();
-  submarine.hit();
-  expect(submarine.isSunk()).toBe(true);
-});
-
-test('denys hits after being sunk', () => {
-  const submarine = new SubMarine();
-  expect(submarine.hit()).toBe(1);
-  expect(submarine.hit()).toBe(1);
-  expect(submarine.hit()).toBe(2);
-  expect(submarine.hit()).toBe(-1);
+  test('should create a submarine class with correct size and name', () => {
+    expect(submarine.size).toBe(3);
+    expect(submarine.name).toBe('submarine');
+  });
 });
