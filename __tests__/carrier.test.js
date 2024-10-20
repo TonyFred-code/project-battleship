21cdc48df9470ede732b2357e3640e52352baf98
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-10-20 04:02:56 +0100

    feat: reconfigure test suite
    test for only class initialization parameters

--- __tests__/carrier.test.js
+++ __tests__/carrier.test.js
@@ -1,36 +1,14 @@
 import Carrier from '../src/carrier.js';
-import Ship from '../src/ship.js';
 
-test('can create a carrier class', () => {
-  expect(new Carrier()).not.toBeUndefined();
-});
+describe('Carrier Class', () => {
+  let carrier;
 
-test('is a ship extension', () => {
-  const carrier = new Carrier();
-  expect(carrier instanceof Ship).toBe(true);
-});
+  beforeEach(() => {
+    carrier = new Carrier();
+  });
 
-test('has a fixed length of 5', () => {
-  const carrier = new Carrier(6);
-  expect(carrier).toHaveLength(5);
-});
-
-test('is sunk in five hits', () => {
-  const carrier = new Carrier();
-  carrier.hit();
-  carrier.hit();
-  carrier.hit();
-  carrier.hit();
-  carrier.hit();
-  expect(carrier.isSunk()).toBe(true);
-});
-
-test('denys hits after being sunk', () => {
-  const carrier = new Carrier();
-  expect(carrier.hit()).toBe(1);
-  expect(carrier.hit()).toBe(1);
-  expect(carrier.hit()).toBe(1);
-  expect(carrier.hit()).toBe(1);
-  expect(carrier.hit()).toBe(2);
-  expect(carrier.hit()).toBe(-1);
+  test('should create a carrier class ship with correct size and name', () => {
+    expect(carrier.size).toBe(5);
+    expect(carrier.name).toBe('carrier');
+  });
 });
