c45065a44b887bc78a9292c08bf79c223edd017e
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-10-20 03:58:45 +0100

    feat: reconfigure test suite
    test for only proper initialization

--- __tests__/destroyer.test.js
+++ __tests__/destroyer.test.js
@@ -1,32 +1,14 @@
 import Destroyer from '../src/destroyer.js';
-import Ship from '../src/ship.js';
 
-test('can create a destroyer class', () => {
-  expect(new Destroyer()).not.toBeUndefined();
-});
+describe('Destroyer class', () => {
+  let destroyer;
 
-test('is a ship extension', () => {
-  const destroyer = new Destroyer();
-  expect(destroyer instanceof Ship).toBe(true);
-});
+  beforeEach(() => {
+    destroyer = new Destroyer();
+  });
 
-test('has a fixed length of 3', () => {
-  const destroyer = new Destroyer(6);
-  expect(destroyer).toHaveLength(3);
-});
-
-test('is sunk in 3 hits', () => {
-  const destroyer = new Destroyer();
-  destroyer.hit();
-  destroyer.hit();
-  destroyer.hit();
-  expect(destroyer.isSunk()).toBe(true);
-});
-
-test('denys hits after being sunk', () => {
-  const destroyer = new Destroyer();
-  expect(destroyer.hit()).toBe(1);
-  expect(destroyer.hit()).toBe(1);
-  expect(destroyer.hit()).toBe(2);
-  expect(destroyer.hit()).toBe(-1);
+  test('should create a destroyer class ship with correct size and name', () => {
+    expect(destroyer.size).toBe(3);
+    expect(destroyer.name).toBe('destroyer');
+  });
 });
